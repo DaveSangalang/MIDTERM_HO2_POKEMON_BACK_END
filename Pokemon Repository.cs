8f4ecec09d0100dba4ef57908a5ba4c4f2b5c859
DaveSangalang <Davesangalang2703@gmail.com>
2025-03-15 12:58:45 +0800

    Add project files.

---
+++ Pokemon Repository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIDTERM_HO2_POKEMON_BACK_END.Pages
+{
+    public class Pokemon_Repository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
